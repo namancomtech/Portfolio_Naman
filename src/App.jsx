3e0fd8ed4d0d06c3d3a31efd1c9615f793c41233
Naman Sharma <namancomtech@gmail.com>
2026-04-20 23:15:43 +0530

    Navbar Update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
 import Contact from './Components/Contact'
 import About from './Components/About'
-import Services from './Components/Services'
+// import Services from './Components/Services'
 import Skills from './Components/Skills'
 import Footer from './Components/Footer'
 import Projects from './Components/Projects'
@@ -18,7 +18,7 @@ function App() {
     <Navbar />
       <Hero />
       <About />
-      <Services />
+      {/* <Services /> */}
        <Skills />
        <Projects />
       <Contact />
